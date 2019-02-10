af779dded3698f6b197de0ebca1433cb92dda6b5
Rahmatsyah <rahmatsyah1317@gmail.com>
2019-02-10 21:22:54 +0700

    Create Event

--- app/src/main/java/com/npe/scheduller/model/database/JadwalHelper.java
+++ app/src/main/java/com/npe/scheduller/model/database/JadwalHelper.java
@@ -1,10 +1,13 @@
 package com.npe.scheduller.model.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.npe.scheduller.model.JadwalModel;
+
+import com.npe.scheduller.model.database.DatabaseContract;
 
 import java.util.ArrayList;
 
@@ -35,12 +38,20 @@ public class JadwalHelper {
     }
 
     public long insert(JadwalModel jadwalModel){
-        return 0;
+        ContentValues initialValues = new ContentValues();
+
+        initialValues.put(DatabaseContract.JadwalColumns.JUDUL, jadwalModel.getJudul());
+        initialValues.put(DatabaseContract.JadwalColumns.REMIND, jadwalModel.getRemind());
+        initialValues.put(DatabaseContract.JadwalColumns.WARNA, jadwalModel.getWarna());
+        initialValues.put(DatabaseContract.JadwalColumns.TIME, jadwalModel.getTime());
+        initialValues.put(DatabaseContract.JadwalColumns.DATE, jadwalModel.getDate());
+
+        return database.insert(DatabaseContract.TABEL_JADWAL,null, initialValues);
     }
     public int update(JadwalModel jadwalModel){
         return 0;
     }
-    public int deleter(int id){
-        return 0;
+    public int deletee(int id){
+        return database.delete(DatabaseContract.TABEL_JADWAL, id + " = '" + id + "'", null);
     }
 }
