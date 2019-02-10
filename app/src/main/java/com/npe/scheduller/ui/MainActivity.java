8fb3700949c5a1c0217cbea95336ebe7d08e308d
digiponic-renal <renaldy@digiponic.co.id>
2019-02-10 14:26:58 +0700

    init

--- app/src/main/java/com/npe/scheduller/ui/MainActivity.java
+++ app/src/main/java/com/npe/scheduller/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.npe.scheduller;
+package com.npe.scheduller.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.npe.scheduller.R;
 
 public class MainActivity extends AppCompatActivity {
 
