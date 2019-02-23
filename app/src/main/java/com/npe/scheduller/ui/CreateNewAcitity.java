8e18ffdee3cf5d8211d116804c7002a4d05b36a9
digiponic-renal <renaldy@digiponic.co.id>
2019-02-24 00:04:54 +0700

    bottom presistance color

--- app/src/main/java/com/npe/scheduller/ui/CreateNewAcitity.java
+++ app/src/main/java/com/npe/scheduller/ui/CreateNewAcitity.java
@@ -1,10 +1,15 @@
 package com.npe.scheduller.ui;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.npe.scheduller.R;
@@ -15,6 +20,8 @@ public class CreateNewAcitity extends AppCompatActivity implements JadwalView.Ja
     private JadwalPresenter presenter;
     Button btnDate, btnTime, btnColor, btnRemind, btnInsert;
     EditText etJudul;
+    LinearLayout layoutBottomColor;
+    BottomSheetBehavior sheetBehaviorColor;
     String judul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +35,83 @@ public class CreateNewAcitity extends AppCompatActivity implements JadwalView.Ja
         btnRemind = findViewById(R.id.btnRemind);
         btnInsert = findViewById(R.id.btnInsert);
         etJudul = findViewById(R.id.etCreateJudul);
+        //color
+        layoutBottomColor = findViewById(R.id.layoutBottomSheetColor);
+        sheetBehaviorColor = BottomSheetBehavior.from(layoutBottomColor);
 
-        //check judul
+        //set sheet color hiden
+        sheetBehaviorColor.setState(BottomSheetBehavior.STATE_HIDDEN);
+        bottomSheetColorBehavior();
+
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.setAlertDate();
+            }
+        });
+
+        btnColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetColor();
+            }
+        });
 
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //check judul
                 judul = etJudul.getText().toString();
-                if(presenter.checkJudul(judul, etJudul)){
+                if (presenter.checkJudul(judul, etJudul)) {
                     Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
+    @Override
+    public void bottomSheetColorBehavior() {
+        sheetBehaviorColor.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+                switch (i){
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void showBottomSheetColor() {
+        if (sheetBehaviorColor.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+            sheetBehaviorColor.setState(BottomSheetBehavior.STATE_EXPANDED);
+        } else {
+            sheetBehaviorColor.setState(BottomSheetBehavior.STATE_HIDDEN);
+        }
+    }
+
+    @Override
+    public void alertDialogDate(int day, int month, int year) {
+
+    }
 
     @Override
     public void insertSuccess(String message) {
