d49a47b722ebf8910ce80fd75485f1e03e8563ee
agenthrn <harun@aku-bantu.com>
2019-02-24 01:04:15 +0700

    Penambahan main activity

--- app/src/main/java/com/npe/scheduller/ui/MainActivity.java
+++ app/src/main/java/com/npe/scheduller/ui/MainActivity.java
@@ -2,16 +2,30 @@ package com.npe.scheduller.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.LinearLayout;
+import java.util.ArrayList;
 import com.npe.scheduller.R;
+import com.npe.scheduller.model.JadwalModel;
+
+import java.util.Calendar;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private Button btnCreate;
+    private static RecyclerView.Adapter adapter;
+    private static ArrayList<JadwalModel> data;
+    LinearLayout layoutBottomSheet,calendarLayoutBottomSheet;
+    BottomSheetBehavior sheetBehavior;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +33,67 @@ public class MainActivity extends AppCompatActivity {
         //inisialisasi
         recyclerView = findViewById(R.id.recyclerViewMain);
         btnCreate = findViewById(R.id.btnCreate);
+        calendarLayoutBottomSheet = findViewById(R.id.calendarBottomSheet);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewMain);
+        data = new ArrayList<>();
+        data.add(new JadwalModel(1,1,"Saturday","07.00","Test"));
+
+        adapter = new AdapterJadwal(data);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(adapter);
+
+        /* starts before 1 month from now */
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.MONTH, -1);
+
+        /* ends after 1 month from now */
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH, 1);
+
+        final HorizontalCalendar horizontalCalendar = new HorizontalCalendar.Builder(this, R.id.calendarView)
+                .range(startDate, endDate)
+                .datesNumberOnScreen(5)
+                .build();
+
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            public void onDateSelected(Calendar date, int position) {
+
+            }
+        });
+
+        layoutBottomSheet = findViewById(R.id.bottom_sheet);
+
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+                switch (i){
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        calendarLayoutBottomSheet.setVisibility(View.VISIBLE);
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        calendarLayoutBottomSheet.setVisibility(View.GONE);
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        calendarLayoutBottomSheet.setVisibility(View.VISIBLE);
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        calendarLayoutBottomSheet.setVisibility(View.VISIBLE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
+            }
+        });
 
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +108,13 @@ public class MainActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
+
+
+
+
+
+
+
 
 
 }
